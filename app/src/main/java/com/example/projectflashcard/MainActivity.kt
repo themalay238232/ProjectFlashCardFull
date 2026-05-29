e91c751c2635a011f378fece020682eb905d1c7a
tnh16092005-ops <tnh16092005@gmail.com>
2026-05-29 23:42:53 +0700

    Them log vong doi Activity (phan 24)

--- app/src/main/java/com/example/projectflashcard/MainActivity.kt
+++ app/src/main/java/com/example/projectflashcard/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.projectflashcard
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -12,5 +13,31 @@ class MainActivity : ComponentActivity() {
         setContent {
             UngDungLearnFlash()
         }
+        Log.d("LearnFlashLifecycle", "onCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("LearnFlashLifecycle", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("LearnFlashLifecycle", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("LearnFlashLifecycle", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("LearnFlashLifecycle", "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("LearnFlashLifecycle", "onDestroy")
     }
 }
