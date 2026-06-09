8b4463dbe5fad38969773d08e368b226909928bb
themalay238232 <longv7393@gmail.com>
2026-06-10 00:42:00 +0700

    Add lifecycle Logcat callbacks

--- app/src/main/java/com/example/projectflashcard/MainActivity.kt
+++ app/src/main/java/com/example/projectflashcard/MainActivity.kt
@@ -1,16 +1,45 @@
 package com.example.projectflashcard
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 
+private const val TAG = "LearnFlashLifecycle"
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate")
         enableEdgeToEdge()
         setContent {
             UngDungLearnFlash()
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy")
     }
 }
